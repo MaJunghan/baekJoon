8f949f587c2d0829483777eb11a112f3bb49e604
MaJunghan <wjdgksak@naver.com>
2022-06-01 15:58:04 +0900

    11718 그대로출력하기

--- 11022/test.js
+++ 11022/test.js
@@ -1,4 +1,4 @@
-let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+let input = require("fs").readFileSync("./test.txt").toString().split("\n");
 
 for (let i = 1; i <= input[0]; i++) {
   let numbers = input[i].split(" ");
